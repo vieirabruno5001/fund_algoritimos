3ee75cba40be4e95f7918b309b851fe37c5fffdb
vieirabruno5001 <vieirabruno5001@gmail.com>
2026-03-26 22:33:01 -0300

    .

---
+++ AULA_6/exemplo1.py
@@ -0,0 +1,7 @@
+for i in range(0, 10):
+    for j in range(0,10):
+       if i == 2:
+        print(i, end='')
+       else:
+          print('', end='')
+    print()
--- exemplo1.py
+++
@@ -1,7 +0,0 @@
-for i in range(0, 10):
-    for j in range(0,10):
-       if i == 2:
-        print(i, end='')
-       else:
-          print('', end='')
-    print()
